223351436b5b5e36b4883b6c27dce56239961fb5
Daniel Haarhoff <d.haarhoff@elifesciences.org>
2022-02-06 13:07:46 +0000

    Hash the input

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,12 @@
 import {sha512} from 'js-sha512'
 
 export function updateResult() {
-  const inputElement = document.getElementById('input')
-
-  const resultElement = document.getElementById('result')
-
-  resultElement.textContent = sha512.hex(inputElement.textContent)
+  resultElement.textContent = sha512.hex(inputElement.value)
 }
+
+const inputElement = document.getElementById('input') as HTMLInputElement
+
+const resultElement = document.getElementById('result')
 
 const updateElement = document.getElementById('update')
 
