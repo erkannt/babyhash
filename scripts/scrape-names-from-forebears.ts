07d56cfde8bd8e036627eada9e47684b3ade24e0
Daniel Haarhoff <d.haarhoff@elifesciences.org>
2022-02-06 14:47:23 +0000

    Seam for scraping incidenc

--- scripts/scrape-names-from-forebears.ts
+++ scripts/scrape-names-from-forebears.ts
@@ -3,18 +3,29 @@ import { data } from './example-data'
 import { JSDOM } from 'jsdom';
 import * as RA from 'fp-ts/ReadonlyArray';
 
+const zip = <A>(arr: ReadonlyArray<A>, ...arrs: ReadonlyArray<ReadonlyArray<A>>) => {
+  return arr.map((val, i) => arrs.reduce((a, arr) => [...a, arr[i]], [val]));
+}
+
 const extractName = (input: DocumentFragment) => pipe(
   input,
   (html) => Array.from(html.querySelectorAll('.name')),
   RA.map((element) => element.innerHTML)
 )
 
+const extractNameAndIncidence = (input: DocumentFragment) => {
+  const names = extractName(input)
+  const incidence = extractName(input)
+
+  return zip(names, incidence)
+}
+
 const scrape = async () => {
 	//const {data} = await (axios.get('https://forebears.io/forenames/begining-with/a'))
 	const result = pipe (
 		data,
     JSDOM.fragment,
-    extractName,
+    extractNameAndIncidence,
 	)
   console.log(result)
 }
