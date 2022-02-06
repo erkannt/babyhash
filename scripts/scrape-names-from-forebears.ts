ec576c493ab3eb2744808a3284521b95c3fb5814
Daniel Haarhoff <d.haarhoff@elifesciences.org>
2022-02-06 15:49:34 +0000

    Prettier

--- scripts/scrape-names-from-forebears.ts
+++ scripts/scrape-names-from-forebears.ts
@@ -1,60 +1,62 @@
-import { flow, pipe } from 'fp-ts/lib/function'
+import { flow, pipe } from 'fp-ts/lib/function';
 import { JSDOM } from 'jsdom';
 import * as RA from 'fp-ts/ReadonlyArray';
-import axios from 'axios'
+import axios from 'axios';
 import * as T from 'fp-ts/Task';
-import * as N from 'fp-ts/number'
-import * as Ord from 'fp-ts/Ord'
-import * as fs from "fs";
+import * as N from 'fp-ts/number';
+import * as Ord from 'fp-ts/Ord';
+import * as fs from 'fs';
 
-const extractName = (input: DocumentFragment) => pipe(
-  input,
-  (html) => Array.from(html.querySelectorAll('.name')),
-  RA.map((element) => element.innerHTML)
-)
+const extractName = (input: DocumentFragment) =>
+  pipe(
+    input,
+    (html) => Array.from(html.querySelectorAll('.name')),
+    RA.map((element) => element.innerHTML),
+  );
 
-const extractIncidence = (input: DocumentFragment) => pipe(
-  input,
-  (html) => Array.from(html.querySelectorAll('.detail-value')),
-  RA.map((element) => element.innerHTML),
-  RA.map((s) => s.replace(/,/g, '')),
-  RA.map((s) => parseInt(s, 10)),
-)
-
+const extractIncidence = (input: DocumentFragment) =>
+  pipe(
+    input,
+    (html) => Array.from(html.querySelectorAll('.detail-value')),
+    RA.map((element) => element.innerHTML),
+    RA.map((s) => s.replace(/,/g, '')),
+    RA.map((s) => parseInt(s, 10)),
+  );
 
 const extractNameAndIncidence = (input: DocumentFragment): ReadonlyArray<NameIncidence> => {
-  const names = extractName(input)
-  const incidence = extractIncidence(input)
+  const names = extractName(input);
+  const incidence = extractIncidence(input);
 
-  return RA.zip(names, incidence)
-}
-
+  return RA.zip(names, incidence);
+};
 
 const getData = (url: string) => async () => {
-  const { data } = await (axios.get(url))
-  return data
-}
+  const { data } = await axios.get(url);
+  return data;
+};
 
-type NameIncidence = Readonly<[string, number]>
+type NameIncidence = Readonly<[string, number]>;
 
 const scrape = async () => {
   const result = await pipe(
     'aáåâäàæãbcçčćdđďeéèfghiíjklłľmnñňoöøóőõðpqrřsşšșśtţþțťuüúūvwxyzžż',
-    (letters) => letters.split(""),
+    (letters) => letters.split(''),
     RA.map(encodeURIComponent),
     RA.map((letter) => `https://forebears.io/forenames/begining-with/${letter}`),
     T.traverseArray(getData),
-    T.map(flow(
-      RA.map(JSDOM.fragment),
-      RA.map(extractNameAndIncidence),
-      RA.flatten,
-      RA.sort(Ord.contramap((tup: NameIncidence) => tup[1])(N.Ord)),
-      RA.reverse,
-      JSON.stringify,
-    ))
-  )()
+    T.map(
+      flow(
+        RA.map(JSDOM.fragment),
+        RA.map(extractNameAndIncidence),
+        RA.flatten,
+        RA.sort(Ord.contramap((tup: NameIncidence) => tup[1])(N.Ord)),
+        RA.reverse,
+        JSON.stringify,
+      ),
+    ),
+  )();
 
-  const outputFile = './data/names-with-incidence.json'
+  const outputFile = './data/names-with-incidence.json';
 
   fs.writeFile(outputFile, result, (err) => {
     if (err) {
@@ -62,6 +64,6 @@ const scrape = async () => {
     }
     console.log(`Saved to '${outputFile}'`);
   });
-}
+};
 
-scrape()
+scrape();
--- src/index.ts
+++ src/index.ts
@@ -1,48 +1,48 @@
-import {sha512} from 'js-sha512'
-import {pipe} from 'fp-ts/function'
+import { sha512 } from 'js-sha512';
+import { pipe } from 'fp-ts/function';
 import * as O from 'fp-ts/Option';
 import { sequenceS } from 'fp-ts/lib/Apply';
 import { top1000 } from './names';
 import * as RA from 'fp-ts/ReadonlyArray';
 
-const chunkString =  (size: number) => (input: string) =>{
-  const inputLenght = input.length
+const chunkString = (size: number) => (input: string) => {
+  const inputLenght = input.length;
   const nChunks = Math.ceil(inputLenght / size);
-  let result = []
+  let result = [];
   for (let i = 0; i < nChunks; i++) {
-    result.push(input.slice(size*i, size*i+size))
+    result.push(input.slice(size * i, size * i + size));
   }
-  return(result.map((s) => parseInt(s, 10)))
-}
+  return result.map((s) => parseInt(s, 10));
+};
 
-const hash = (input: string) => pipe(
-  input,
-  sha512.hex,
-  (hex) => '0x' + hex,
-  BigInt,
-  String,
-  chunkString(3),
-  RA.map((i) => top1000[i]),
-  (names) => names.join(' ')
-)
+const hash = (input: string) =>
+  pipe(
+    input,
+    sha512.hex,
+    (hex) => '0x' + hex,
+    BigInt,
+    String,
+    chunkString(3),
+    RA.map((i) => top1000[i]),
+    (names) => names.join(' '),
+  );
 
 const updateResult = (result: HTMLElement, input: HTMLInputElement) => () => {
-  result.textContent = hash(input.value)
-}
+  result.textContent = hash(input.value);
+};
 
 pipe(
   {
-    input:  O.fromNullable(document.getElementById('input') as HTMLInputElement),
-    button:  O.fromNullable(document.getElementById('update')),
-    result:  O.fromNullable(document.getElementById('result')),
+    input: O.fromNullable(document.getElementById('input') as HTMLInputElement),
+    button: O.fromNullable(document.getElementById('update')),
+    result: O.fromNullable(document.getElementById('result')),
   },
   sequenceS(O.Apply),
   O.match(
     () => {},
-    ({input, button, result}) => {
-      updateResult(result, input)()
-      button.addEventListener('click', updateResult(result, input))
-    }
-  )
-)
-
+    ({ input, button, result }) => {
+      updateResult(result, input)();
+      button.addEventListener('click', updateResult(result, input));
+    },
+  ),
+);
